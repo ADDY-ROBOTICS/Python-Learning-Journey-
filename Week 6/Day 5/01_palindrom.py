d711820ed87de61e460455f7aa626fb7db0da66b
ADDY-ROBOTICS <adityapandey.robotics@gmail.com>
2025-08-20 13:42:29 +0530

    Start of Week 6

---
+++ Week 6/Day 5/01_palindrom.py
@@ -0,0 +1,8 @@
+#Write a Python program to check if "madam" is a palindrome using [::-1] slicing.
+def main():
+    s = "madam"
+    print(s == s[::-1])
+    
+if __name__ == "__main__":
+    main()
+    
